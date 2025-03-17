f670a060218c383f5b6216c12dad4fcf1591c1fb
AladinJmila <jmila.aladin@gmail.com>
2025-03-17 23:29:10 +0100

    Add zoom functionality to body map

--- app/BodyMap.tsx
+++ app/BodyMap.tsx
@@ -1,29 +1,49 @@
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  GestureEvent,
+  GestureHandlerRootView,
+  PinchGestureHandler,
+  PinchGestureHandlerEventPayload,
+  State,
+} from "react-native-gesture-handler";
 import Svg from "react-native-svg";
 import BodyPart from "./BodyPart";
 
 import { backSide, frontSide } from "@/services/bodyParts";
 
 const BodyMap = () => {
+  const [scale, setScale] = useState(1);
+
+  const onPinchEvent = (event: GestureEvent<PinchGestureHandlerEventPayload>) => {
+    setScale(event.nativeEvent.scale);
+  };
+
+  const onPinchStateChange = (event: GestureEvent<PinchGestureHandlerEventPayload>) => {
+    if (event.nativeEvent.state === State.END) setScale(event.nativeEvent.scale);
+  };
   const [flip, setFlip] = useState(true);
   return (
-    <View style={styles.container}>
-      <Svg
-        width="90%"
-        height="720"
-        viewBox="870 500 3000 5000"
-        preserveAspectRatio="xMidYMid meet"
-        transform="scale(1, -1)"
-      >
-        {flip
-          ? frontSide.map((part) => <BodyPart key={part.id} data={part} />)
-          : backSide.map((part) => <BodyPart key={part.id} data={part} />)}
-      </Svg>
-      <TouchableOpacity style={styles.button} onPress={() => setFlip((prev) => !prev)}>
-        <Text style={{ color: "#fff" }}>Flip</Text>
-      </TouchableOpacity>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <PinchGestureHandler onGestureEvent={onPinchEvent} onHandlerStateChange={onPinchStateChange}>
+          <Svg
+            width="90%"
+            height="720"
+            viewBox="870 500 3000 5000"
+            preserveAspectRatio="xMidYMid meet"
+            style={{ transform: [{ scale }, { scaleX: 1 }, { scaleY: -1 }] }}
+          >
+            {flip
+              ? frontSide.map((part) => <BodyPart key={part.id} data={part} />)
+              : backSide.map((part) => <BodyPart key={part.id} data={part} />)}
+          </Svg>
+        </PinchGestureHandler>
+        <TouchableOpacity style={styles.button} onPress={() => setFlip((prev) => !prev)}>
+          <Text style={{ color: "#fff" }}>Flip</Text>
+        </TouchableOpacity>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
