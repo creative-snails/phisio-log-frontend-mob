96435731d42a6e7e291e612c8ba3b2801de4f417
sttadic <stjepan.tadich@gmail.com>
2025-04-01 23:58:02 +0100

    Implement loading state and error handling during save

--- app/EditTreatments.tsx
+++ app/EditTreatments.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import { Alert, Platform, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { router } from "expo-router";
 
 import useAppStore from "@/store/useAppStore";
 
 const EditTreatments = () => {
-  const { setHealthRecord, healthRecord } = useAppStore();
+  const { setHealthRecord, healthRecord, setLoading, loading } = useAppStore();
   const [localTreatments, setLocalTreatments] = useState(healthRecord.treatmentsTried);
 
   const handleTreatmentChange = (text: string, index: number) => {
@@ -18,14 +28,31 @@ const EditTreatments = () => {
       if (Platform.OS === "web") {
         window.alert("Treatment name must be at least 3 characters long!");
       } else {
-        Alert.alert("Treatment name must be at least 3 characters long!");
+        Alert.alert("Error", "Treatment name must be at least 3 characters long!");
       }
       return;
     }
-    setHealthRecord({ ...healthRecord, treatmentsTried: localTreatments });
-    console.log("Saved treatments:", localTreatments);
-    router.back();
+    try {
+      setLoading(true);
+      setHealthRecord({ ...healthRecord, treatmentsTried: localTreatments });
+      console.log("Saved treatments: ", localTreatments);
+      router.back();
+    } catch (error) {
+      console.error("Error saving treatments:", error);
+      Alert.alert("Error", "Failed to save treatments");
+    } finally {
+      setLoading(false);
+    }
   };
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#FBDABB" />
+        <Text style={styles.loadingText}>Saving changes...</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView>
@@ -52,6 +79,15 @@ const EditTreatments = () => {
 };
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    alignItems: "center",
+    height: "100%",
+    justifyContent: "center",
+  },
+  loadingText: {
+    fontSize: 20,
+    marginTop: 10,
+  },
   saveBtn: {
     alignItems: "center",
     backgroundColor: "#FBDABB",
