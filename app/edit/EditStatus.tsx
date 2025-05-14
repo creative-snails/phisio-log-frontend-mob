d34507af4e0bfb12d7a989665e0cccd09eed84f8
sttadic <stjepan.tadich@gmail.com>
2025-05-14 12:31:28 +0100

    Implement logic to map through statusConfigs to render dropdowns dynamically

--- app/edit/EditStatus.tsx
+++ app/edit/EditStatus.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 
 import { SaveCancelButtons } from "@/components/formElements/SaveCancelButtons";
 import { useFormEdit } from "@/hooks/useFormEdit";
 import { StatusOptionsType } from "@/types/healthRecordTypes";
-import { statusOptions } from "@/utils/constants";
+import { statusConfigs, statusOptions } from "@/utils/constants";
 import { validators } from "@/utils/validators";
 
 const EditStatus = () => {
@@ -23,45 +23,24 @@ const EditStatus = () => {
 
   return (
     <View style={styles.statusContainer}>
-      <Text style={styles.title}>Edit Status</Text>
-      <DropDownPicker
-        open={openDropdown === "stage"}
-        setOpen={() => handleDropdowns("stage")}
-        value={localValue.stage || null}
-        items={statusOptions.stage}
-        setValue={(val) => updateStatus("stage", val(localValue.stage))}
-        zIndex={3000}
-        zIndexInverse={1000}
-        containerStyle={styles.dropdown}
-        textStyle={styles.items}
-        labelStyle={styles.selectedItem}
-      />
-      <Text style={styles.title}>Edit Severity</Text>
-      <DropDownPicker
-        open={openDropdown === "severity"}
-        setOpen={() => handleDropdowns("severity")}
-        value={localValue.severity || null}
-        items={statusOptions.severity}
-        setValue={(val) => updateStatus("severity", val(localValue.severity))}
-        zIndex={2000}
-        zIndexInverse={2000}
-        containerStyle={styles.dropdown}
-        textStyle={styles.items}
-        labelStyle={styles.selectedItem}
-      />
-      <Text style={styles.title}>Edit Improvement Status</Text>
-      <DropDownPicker
-        open={openDropdown === "progression"}
-        setOpen={() => handleDropdowns("progression")}
-        value={localValue.progression || null}
-        items={statusOptions.progression}
-        setValue={(val) => updateStatus("progression", val(localValue.progression))}
-        zIndex={1000}
-        zIndexInverse={3000}
-        containerStyle={styles.dropdown}
-        textStyle={styles.items}
-        labelStyle={styles.selectedItem}
-      />
+      {statusConfigs.map(({ field, title, zIndex, zIndexInverse }) => (
+        <View key={field}>
+          <Text style={styles.title}>Edit {title}</Text>
+          <DropDownPicker
+            open={openDropdown === field}
+            setOpen={() => handleDropdowns(field)}
+            value={localValue[field] || null}
+            items={statusOptions[field]}
+            setValue={(val) => updateStatus(field, val(localValue[field]))}
+            zIndex={zIndex}
+            zIndexInverse={zIndexInverse}
+            containerStyle={styles.dropdown}
+            textStyle={styles.items}
+            labelStyle={styles.selectedItem}
+            listMode={Platform.OS === "web" ? "MODAL" : "FLATLIST"}
+          />
+        </View>
+      ))}
       <SaveCancelButtons onSave={handleSave} />
     </View>
   );
