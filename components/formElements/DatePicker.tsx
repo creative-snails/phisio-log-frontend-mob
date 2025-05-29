df58e78a958393f2b3d8bbb47430b5d12ffa7b19
Daniel B <daarf92@gmail.com>
2025-05-29 13:25:28 +0100

    Date Picker adjusted

--- components/formElements/DatePicker.tsx
+++ components/formElements/DatePicker.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { DatePickerModal, enGB, registerTranslation } from "react-native-paper-dates";
 import { CalendarDate } from "react-native-paper-dates/lib/typescript/Date/Calendar";
+
+import CustomButton from "@/components/Button";
 
 registerTranslation("en-GB", enGB);
 
@@ -16,9 +17,7 @@ type DatePickerProps = {
 export const DatePicker = ({ isOpen, onDismiss, onConfirm, date, value, onPress }: DatePickerProps) => {
   return (
     <>
-      <TouchableOpacity style={styles.dateBtn} onPress={onPress}>
-        <Text style={styles.dateText}>{value ? value.toString() : "Select Date"}</Text>
-      </TouchableOpacity>
+      <CustomButton title={value ? value.toString() : "Select Date"} onPress={onPress} />
       <DatePickerModal
         locale="en-GB"
         mode="single"
@@ -32,20 +31,3 @@ export const DatePicker = ({ isOpen, onDismiss, onConfirm, date, value, onPress 
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  dateBtn: {
-    backgroundColor: "#afd0e3",
-    borderRadius: 10,
-    boxShadow: "2px 2px 0px #000",
-    marginHorizontal: "auto",
-    marginVertical: 15,
-    padding: 5,
-    width: "auto",
-  },
-  dateText: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    textAlign: "center",
-  },
-});
