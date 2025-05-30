11bfa48c024e09b410d43f2e8be02e3f033a9dd9
Daniel B <daarf92@gmail.com>
2025-05-30 10:48:18 +0100

    Commented out code removed

--- components/formElements/SaveCancelButtons.tsx
+++ components/formElements/SaveCancelButtons.tsx
@@ -7,13 +7,7 @@ import { commonStyles } from "@/styles/commonStyles";
 export const SaveCancelButtons = ({ onSave }: { onSave: () => void }) => {
   return (
     <View style={commonStyles.btnContainer}>
-      {/* <Pressable style={commonStyles.btn} onPress={onSave}>
-        <Text style={commonStyles.btnText}>Save</Text>
-      </Pressable> */}
       <CustomButton title="Save" onPress={onSave} />
-      {/* <Pressable style={commonStyles.btn} onPress={router.back}>
-        <Text style={commonStyles.btnText}>Cancel</Text>
-      </Pressable> */}
       <CustomButton title="Cancel" variant="secondary" onPress={router.back} />
     </View>
   );
